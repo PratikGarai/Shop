aedfed6df698f80fe5a21cce755bd746efe6a303
Pratik Garai <pratikgarai0208@gmail.com>
2020-06-28 07:46:30 +0530

    Added request handler for 404

--- app.js
+++ app.js
@@ -10,4 +10,7 @@ app.use(bodyParser.urlencoded({ 'extended' : false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use('/', (req, res, next)=>{
+	res.status(404).send("<h1>Page Not Found!</h1>");
+});
 app.listen(3000);
