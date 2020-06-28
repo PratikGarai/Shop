12d2cdac301c046a30b2ab53481c49e2793e768c
Pratik Garai <pratikgarai0208@gmail.com>
2020-06-28 07:04:44 +0530

    Created directory for routing

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Shop-Clone-Node
 A shopping website clone using NodeJS
+
+Directory Explanation : <br>
+
+/app.js                :  Main module of the entire folder <br>
+
+/routes                :  Stores the url routing files<br>
+/routes/admin.js       :  Links available to shop admin<br>
+/routes/shop.js        :  Links available to shop visitors<br>
--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/add-product', (req, res, next)=> {
-	res.send("<h1>This is add-product page.</h1>");
+app.use(bodyParser.urlencoded({ 'extended' : false}));
+
+app.get('/add-product', (req, res, next)=> {
+	res.send('<html><head><title>Form</title></head><body><form method = "POST" action = "/product" ><input type = "text" name = "sometext"><input type="submit"></form></body></html>');
+});
+
+app.post('/product', (req, res, next)=> {
+	console.log(req.body);
+	res.redirect('/');
 });
 
 app.use('/', (req, res, next)=> {
