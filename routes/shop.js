f0af5ba9158c925a44409cd5649ee6d7868316de
Pratik Garai <pratikgarai0208@gmail.com>
2020-06-28 07:39:40 +0530

    Removed use() completely

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.use('/', (req, res, next)=> {
+router.get('/', (req, res, next)=> {
 	res.send("<h1>Welcome to HomePage!</h1>");
 });
 
