fce4fe6fff785853f2bb7eb926cede77e518a407
Pratik Garai <pratikgarai0208@gmail.com>
2020-06-29 16:34:10 +0530

    Added pages and styling

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ 'extended' : false}));
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next)=>{
-	res.status(404).send("<h1>Page Not Found!</h1>");
+	res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next)=> {
-	res.send('<html><head><title>Form</title></head><body><form method = "POST" action = "/product" ><input type = "text" name = "sometext"><input type="submit"></form></body></html>');
+	res.sendFile(path.join(__dirname,'..','views','add-product.html'));
 });
 
 router.post('/product', (req, res, next)=> {
