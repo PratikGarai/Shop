5c19aec22ec8b91bc0dd0bcb7af097d101751b5d
Pratik Garai <pratikgarai0208@gmail.com>
2020-06-29 17:19:30 +0530

    Moved css to folders

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ 'extended' : false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(express.static( path.join(__dirname,'public') ));
 
 app.use('/', (req, res, next)=>{
 	res.status(404).sendFile(path.join(__dirname,'views','404.html'));
