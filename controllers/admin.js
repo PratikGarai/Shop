089b7e14620903189eddb3e3ce88a28b72705d0f
Pratik Garai <pratikgarai0208@gmail.com>
2020-07-01 02:40:11 +0530

    A few pages configured

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,7 +8,11 @@ exports.getAddProduct = (req, res, next)=> {
 };
 
 exports.postAddProduct = (req, res, next)=> {
-	const product = new Product(req.body.title);
+	const title = req.body.title;
+	const imageUrl = req.body.imageUrl;
+	const price = req.body.price;
+	const description = req.body.description;
+	const product = new Product(title, imageUrl, description, price);
 	product.save();
 	res.redirect('/');
 };
--- models/product.js
+++ models/product.js
@@ -3,8 +3,11 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = class Product {
-	constructor(t){
-		this.title = t;
+	constructor(title, imageUrl, description, price){
+		this.title = title;
+		this.imageUrl = imageUrl;
+		this.description = description;
+		this.price = price;
 	}
 
 	save(){
