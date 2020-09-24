2e74dd6778c9343201ff53ee02ac55a94457ddd1
PratikGarai <pratikgarai0208@gmail.com>
2020-09-24 22:39:36 +0530

    Added code to insert data to database

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,18 @@ exports.postAddProduct = (req, res, next)=> {
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-	const product = new Product(title, imageUrl, description, price);
-	product
-		.save()
-		.then(()=> {
-			res.redirect("/admin/products");
+	Product.create({
+		title : title,
+		price : price,
+		imageUrl : imageUrl,
+		description : description
+	})
+		.then( result => {
+			console.log(result);
 		})
-		.catch((err)=> console.log(err));
+		.catch(err => {
+			console.log(err)
+		});
 };
 
 exports.getEditProduct = (req, res, next)=> {
