e52e688b6e27cb2d63edcc9ee925d278bd507523
PratikGarai <pratikgarai0208@gmail.com>
2020-09-24 14:59:47 +0530

    The new products model is made

---
+++ models/product.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product' , {
+	id : {
+		type : Sequelize.INTEGER,
+		autoIncrement : true,
+		allowNull : false,
+		primaryKey : true
+	},
+	title : Sequelize.STRING,
+	price : {
+		type : Sequelize.DOUBLE,
+		allowNull : false,
+	},
+	imageUrl : {
+		type : Sequelize.STRING(500),
+		allowNull : false,
+	},
+	description : {
+		type : Sequelize.STRING(500),
+		allowNull : false
+	}
+});
+
+module.exports = Product;
