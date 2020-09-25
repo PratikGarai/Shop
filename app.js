363009b00ab6e6d3641bcbd90910bdcebb2ef031
PratikGarai <pratikgarai0208@gmail.com>
2020-09-25 13:50:08 +0530

    Added dummy user code

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const shopRoutes = require('./routes/shop.js');
 const path = require("path");
 const globalController = require('./controllers/globalPages');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,11 +21,29 @@ app.use(express.static( path.join(__dirname,'public') ));
 
 app.use('/', globalController.get404 );
 
+Product.belongsTo(User, { constraints : true, onDelete : 'CASCADE' } );
+User.hasMany(Product);
+
 sequelize
+	// .sync({force : true})
 	.sync()
 	.then( result => {
-		app.listen(3000);
 		// console.log(result);
+		return User.findByPk(1);
+	})
+	.then( user => {
+		if(!user)
+		{
+			console.log("Creating new User");
+			return User.create({ name:'Test User', email : 'test@test.com' });
+		}
+		console.log("Dummy user already exists");
+		return user;
+	})
+	.then( user => {
+		// console.log(user);
+		console.log("Starting server....");
+		app.listen(3000);
 	})
 	.catch( err => {
 		console.log(err);
