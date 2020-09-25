81906d61ce2308ef5c7de7260276f89681ac79c2
PratikGarai <pratikgarai0208@gmail.com>
2020-09-25 17:16:50 +0530

    random commit

--- app.js
+++ app.js
@@ -15,9 +15,21 @@ app.set( "view engine" , "ejs");
 app.set( "views" , "views" );
 
 app.use(bodyParser.urlencoded({ 'extended' : false}));
+app.use(express.static( path.join(__dirname,'public') ));
+
+app.use((req, res, next)=>{
+	User
+		.findByPk(1)
+		.then( user=> {
+			req.user = user;
+			console.log("Binded user to request");
+			next();
+		})
+		.catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(express.static( path.join(__dirname,'public') ));
 
 app.use('/', globalController.get404 );
 
