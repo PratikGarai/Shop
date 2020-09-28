2404cab882ec933557f955411dafa83acd5e53dc
PratikGarai <pratikgarai0208@gmail.com>
2020-09-29 02:58:35 +0530

    New db code

--- util/database_template_mongo.js
+++ util/database_template_mongo.js
@@ -1,16 +1,29 @@
 const mongodb  = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db ;
+
 const mongoConnect = callback => {
 	const uri = "mongodb+srv://Pratik:<password>@pratikcluster.2hp8l.mongodb.net/<dbname>?retryWrites=true&w=majority";
 	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 	client.connect(err => {
 		const collection = client.db("test").collection("devices");
   		client.close();
+		throw err;
 	});	
 	console.log("Connected");
+	_db = client.db();
 	callback(client);
 	return client;
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+	if(_db)
+	{
+		return (_db);
+	}
+	throw "No database found";
+}
+
+exports.mongoConnect = mongoConnect;    //responsible to store the connection
+exports.getDb = getDb; 			//responsible to give access to the db
