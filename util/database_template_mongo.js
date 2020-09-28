a05d410e04c77cb7245c2f5ec10aaea611b3dd9e
PratikGarai <pratikgarai0208@gmail.com>
2020-09-28 21:02:40 +0530

    Cleaning and proper connection code

--- util/database_template_mongo.js
+++ util/database_template_mongo.js
@@ -2,12 +2,15 @@ const mongodb  = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = callback => {
-	MongoClient.connect('mongodb+srv://Pratik:<password>@pratikcluster.2hp8l.mongodb.net/<dbname>?retryWrites=true&w=majority')
-		.then(client =>{
-			console.log('Connected!');
-			callback(client);
-		})
-		.catch(err => console.log(err));
+	const uri = "mongodb+srv://Pratik:<password>@pratikcluster.2hp8l.mongodb.net/<dbname>?retryWrites=true&w=majority";
+	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+	client.connect(err => {
+		const collection = client.db("test").collection("devices");
+  		client.close();
+	});	
+	console.log("Connected");
+	callback(client);
+	return client;
 }
 
 module.exports = mongoConnect;
