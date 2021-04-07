0c098ed2b019fcfb8181b1c3b3535381f8d2847c
PratikGarai <pratikgarai0208@gmail.com>
2021-04-08 03:29:44 +0530

    setting up sessions store

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
+// Library imports
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require("path");
 const mongoose  = require("mongoose");
+const session = require('express-session');
+const MongoStore = require("connect-mongodb-session")(session);
 
+// Routes and controllers and models imports
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 const authRoutes = require('./routes/auth.js');
-
 const globalController = require('./controllers/globalPages');
-
 const User = require('./models/user');
 
-
+// App initialization
 const app = express();
+const MONGODB_URI = "mongodb+srv://pratik:pratik@cluster0.cflq7.gcp.mongodb.net/shop?retryWrites=true&w=majority";
+const store = new MongoStore({
+	uri : MONGODB_URI,
+	collection : "sessions",
+})
 
+// Setting engines
 app.set( "view engine" , "ejs");
 app.set( "views" , "views" );
 
+// Middlewares
 app.use(bodyParser.urlencoded({ 'extended' : false}));
 app.use(express.static( path.join(__dirname,'public') ));
+app.use(session({
+	secret : "pratik's secret",
+	resave : false,
+	saveUninitialized : false,
+	store : store,
+}))
 
 app.use((req, res, next)=>{
-	
+
 	// The simple cookie example
 	const cookie_strings = req.get('Cookie').split(';');
     const cookies = {};
@@ -31,7 +46,7 @@ app.use((req, res, next)=>{
         cookies[key] = value;
     })
     console.log("Cookies : \n", cookies);
-	req.isLoggedIn = cookies.loggedIn;
+	req.isLoggedIn = cookies.loggedIn==='true';
 
 	User
 		.findById('5fc21c8d01c44b545c817bc0')
@@ -44,13 +59,17 @@ app.use((req, res, next)=>{
 		});
 });
 
+// Adding Routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
-
 app.use('/', globalController.get404 );
 
-mongoose.connect("mongodb+srv://pratik:pratik@cluster0.cflq7.gcp.mongodb.net/shop?retryWrites=true&w=majority")
+// Connect to database and start app
+mongoose.connect(MONGODB_URI, {
+			useNewUrlParser : true,
+			useUnifiedTopology : true,
+		})
 		.then( res => {
 			User
 			  .findOne()
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
 exports.getLogin  = (req, res, next) => {
+    console.log(req.session);
     res.render('auth/login', {
         path : '/login',
         pageTitle : 'Login',
-        isLoggedIn : req.isLoggedIn,
+        isLoggedIn : req.session.isLoggedIn,
     });
 }
 
 exports.postLogin  = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true');
+    req.session.isLoggedIn = true;
     res.redirect('/');
 }
