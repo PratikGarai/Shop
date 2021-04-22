455e81dc52c9d3dc87b89b6642ab620413e5b8e2
PratikGarai <pratikgarai0208@gmail.com>
2021-04-22 09:17:23 +0530

    Added auth middleware

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const path = require("path");
 const adminController  = require("../controllers/admin");
+const isAuth = require("../middlewares/isAuth");
 
 const router = express.Router();
 
-router.get('/products', adminController.getProducts);
-router.get('/add-product',adminController.getAddProduct);
-router.post('/add-product', adminController.postAddProduct);
-router.get('/edit-product/:productId',adminController.getEditProduct);
-router.post('/edit-product/:productId',adminController.postEditProduct);
-router.post('/delete-product/:productId',adminController.postDeleteProduct);
+router.get('/products',isAuth, adminController.getProducts);
+router.get('/add-product', isAuth, adminController.getAddProduct);
+router.post('/add-product', isAuth, adminController.postAddProduct);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+router.post('/edit-product/:productId', isAuth, adminController.postEditProduct);
+router.post('/delete-product/:productId', isAuth,adminController.postDeleteProduct);
 
 module.exports = router;
