6221ff8070ec176374c21bd3708ee2afb0d62acc
PratikGarai <pratikgarai0208@gmail.com>
2021-05-28 14:17:41 +0530

    Image static serve

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.set( "views" , "views" );
 app.use(bodyParser.urlencoded({ 'extended' : false}));
 app.use(multer({storage : fileStorage, fileFilter : fileFilter }).single('image'));
 app.use(express.static( path.join(__dirname,'public') ));
+app.use("/images", express.static( path.join(__dirname,'images') ));
 app.use(session({
 	secret : "pratik's secret",
 	resave : false,
